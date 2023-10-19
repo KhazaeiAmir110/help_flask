8f555aa5a049c80cd7cf85bb560f5f4ca1eba55e
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-10-19 07:54:16 +0330

    start work

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/home')
+def hello_world():
+    return render_template('home.html', name='amir')
