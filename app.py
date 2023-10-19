83d217b8d177bbab1f40e2d2a88afafda3b2d9b4
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-10-19 08:15:17 +0330

    update html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return render_template('home.html', name='amir')
+    return render_template('home.html', name='amir', title='home')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='about')
