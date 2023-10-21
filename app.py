7f1fc2eb7201039e68da5da8a254a50300e6ddcc
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-10-21 11:47:40 +0330

    add files html and create views

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/<string:name>')  # address type one
+def home(name):
+    context = {
+        'name': name,
+        'title': 'home',
+        'active_home': 'active',
+    }
+    return render_template('home.html', **context)
 
+
+def about():
+    context = {
+        'title': 'about',
+        'name': 'about',
+        'active_about': 'active',
+    }
+    return render_template('home.html', **context)
+
+
+app.add_url_rule(rule='/about/', view_func=about)  # address type two
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
